81d1ab838986799231c49565fa0606c39ed0999b
Ramya R <ramyawork22@gmail.com>
2024-12-25 14:04:51 +0530

    Added jenkins CICD file

---
+++ jenkins/cicd.groovy
@@ -0,0 +1,32 @@
+pipeline{
+  agent { label "dev" };
+    stages{
+      stage("Fetch the code"){
+        steps{
+          git url: "", branch: "master"
+        }
+    }
+      stage("Build the code"){
+        steps{
+          sh "docker build -t bankapp:latest . "
+        }
+    }
+      stage("Push the image to DockerHub"){
+        steps{
+          withCredentials([usernamePassword(
+            credentialsId:"DockerHub",
+            usernameVariable:"docUser",
+            passwordVariable:"docPasswd"
+          )]){
+            sh "echo $docPasswd | docker login -u $docUser --password-stdin"
+            sh "docker image ag bankapp:latest ${env.docUser}/bankapp:latest"
+            sh "docker push ${env.docUser}/bankapp:latest"
+          }
+        }
+    }
+    stage("Deploy the code"){
+        steps{
+          sh "docker-compose down && docker-compose up -d --build bankapp"
+        }
+    }
+    } 
